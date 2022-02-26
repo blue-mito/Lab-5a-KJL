a3505bbf9598383722d2f83a18067379e3fcd6c2
Ken Lindsay <blue.mitochondrion@gmail.com>
2022-02-26 14:43:03 -0800

    Updated sum and multiplication for question three.

--- js/app.js
+++ js/app.js
@@ -9,11 +9,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+    let sumNum = (parseInt(a) + parseInt(b));
+    let sumArray = [sumNum, `The sum of ${a} and ${b} is ${sumNum}.`];
+    return sumArray;
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -27,11 +29,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+    let multiNum = (parseInt(a) + parseInt(b));
+    let multiArray = [multiNum, `The product of ${a} and ${b} is ${sumNum}.`];
+    return multiArray;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -48,11 +52,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    let sumNum2 = (parseInt(a)+parseInt(b)+parseInt(c));
+    let multiNum2 = (parseInt(a)*parseInt(b)*parseInt(c));
+    let sumMultiArray = [sumNum2, multiNum2, `Third element: ${a} and ${b} and ${c} sum to ${sumNum2}.`, `The product of ${a} and ${b} and ${c} is ${multiNum2}.`];
+    return sumMultiArray;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
